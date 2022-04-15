667c5de0e1dbeb8a2b2575f121bef912936a31a8
Bhavana <bhavananerlakota@gmail.com>
2022-04-16 04:21:37 +0530

    [Bhavana]Added: Max value using sort method

---
+++ GenericsAssignment/MaxMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    class MaxMethod<T>
+    {
+        public int[] arr= new int[5];
+        public T[] value;
+
+        public MaxMethod(T[]value)
+        {
+            this.value = value;
+        }
+        
+        public T[] Sort(T[]value)
+        {
+            Array.Sort(this.value, value);
+            return value;
+        }
+
+        public T FindMax()
+        {
+            T[] res = Sort(value);
+            return res[res.Length - 1];
+        }
+
+        public void PrintResult()
+        {
+            T res = FindMax();
+            Console.WriteLine("Max value is:"+res);
+        }
+        
+
+        
+    }
+}
--- GenericsAssignment/Program.cs
+++ GenericsAssignment/Program.cs
@@ -6,8 +6,9 @@ namespace GenericsAssignment
     {
         static void Main(string[] args)
         {
+            int[] arr = { 20, 25, 35, 40, 55 };
             Console.WriteLine("Welcome to Generics Program\n");
-            Console.WriteLine("1:FindMaximum\n2:FindMaxFloatValues\n3:FindMaxString\n4:GenericFindMax");
+            Console.WriteLine("1:FindMaximum\n2:FindMaxFloatValues\n3:FindMaxString\n4:GenericFindMax\n5:Max Value using sorting");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -27,6 +28,9 @@ namespace GenericsAssignment
                     new GenericMax<int>(20, 30, 40).TestMaximum();
                     new GenericMax<float>(20.6f, 30.9f, 40.3f).TestMaximum();
                     new GenericMax<string>("train","bus","car").TestMaximum();
+                    break;
+                case 5:
+                    new MaxMethod<int>(arr).PrintResult();
                     break;
 
             }
