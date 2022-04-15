19db2d3e2ec21fd71fb45056cef31bfa358d26a3
Bhavana <bhavananerlakota@gmail.com>
2022-04-16 01:02:33 +0530

    [Bhavana]:Added Maximum of 3 integers program

---
+++ GenericsAssignment/FindMaximum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    internal class FindMaximum
+    {
+        public static int FindMaxInteger(int first, int second, int third) 
+        {
+            //to check if first number is maximum or not
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 || first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 || first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("first number is maximum" + first);
+                return first;
+            }
+            //to check if second number is maximum or not
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 || second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 || second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("second number is maximum" + second);
+                return second;
+            }
+            //to check if third number is maximum or not
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 || third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 || third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine("third number is maximum" + third);
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("2 or 3 values might be equal");
+                return 0;
+            }
+        }
+    }
+}
+ 
--- GenericsAssignment/Program.cs
+++ GenericsAssignment/Program.cs
@@ -6,7 +6,17 @@ namespace GenericsAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Program");
+            Console.WriteLine("Welcome to Generics Program\n");
+            Console.WriteLine("1:FindMaximum");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    int result = FindMaximum.FindMaxInteger(56, 45, 30);
+                    Console.WriteLine(result);
+                    break;
+
+            }
         }
     }
 }
