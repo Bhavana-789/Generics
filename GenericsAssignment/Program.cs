89cdd565858e3dcad14fbc714d8398de9ac0724b
Bhavana <bhavananerlakota@gmail.com>
2022-04-16 01:32:01 +0530

    [Bhavana]:Added Max float value program

--- GenericsAssignment/Program.cs
+++ GenericsAssignment/Program.cs
@@ -7,14 +7,19 @@ namespace GenericsAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Program\n");
-            Console.WriteLine("1:FindMaximum");
+            Console.WriteLine("1:FindMaximum\n 2:FindMaxFloatValues\n");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
-                    int result = FindMaximum.FindMaxInteger(56, 45, 30);
+                    int result = FindMaximum.FindMax(56, 45, 30);
                     Console.WriteLine(result);
                     break;
+                case 2:
+                    float result1= FindMaximum.FindMax(56.22f, 45.3f, 30.5f);
+                    Console.WriteLine(result1);
+                    break;
+
 
             }
         }
